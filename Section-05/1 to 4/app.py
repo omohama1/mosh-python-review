e5ac02a2cdf6f76b2d34e161f42412a522ec90a9
Omar Qasirov <omarbmohamad@gmail.com>
2024-04-11 15:32:51 -0500

    adding lessons

--- Section-05/1 to 4/app.py
+++ Section-05/1 to 4/app.py
@@ -33,5 +33,8 @@ first, *other, last = numbers
 
 letters = ["a", "b", "c"]
 
-for letter in enumerate(letters):
-    print(letter)
+# for letter in letters:
+    # print(letter)
+
+# for index, letter in enumerate(letters):
+    # print(index, letter)
