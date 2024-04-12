c71517149a6d68b26f32789281d0c987ea79989d
Omar Bin Mohamad <omarbmohamad@gmail.com>
2024-04-11 19:11:48 -0500

    updating map function program

--- Section-05/14-queues/app.py
+++
@@ -1,1 +0,0 @@
-items = [("Product1", 10), ("Product2", 9), ("Product3", 12)]
--- Section-05/9-map-function/app.py
+++ Section-05/9-map-function/app.py
@@ -1,1 +1,11 @@
-items = [("Product1", 10), ("Product2", 9), ("Product3", 12)]
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12)]
+
+
+prices = []
+for item in items:
+    prices.append(item[1])
+
+print(prices)
