dab1e167208d227d76cdf0570eaffd8075808ec3
Omar Bin Mohamad <omarbmohamad@gmail.com>
2024-04-12 13:47:24 -0500

    adding list comprehension

--- Section-05/11-list-comprehensions/app.py
+++ Section-05/11-list-comprehensions/app.py
@@ -1,1 +1,18 @@
-items = [("Product1", 10), ("Product2", 9), ("Product3", 12)]
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12)]
+
+pricesList = list(map(lambda item: item[1], items))
+print(pricesList)
+
+# Map using list comprehension
+prices = [item[1] for item in items]
+print(prices)
+
+filtered = list(filter(lambda item: item[1] >= 10, items))
+print(filtered)
+
+# Filter using list comprehensions
+filteredComprehension = [item for item in items if item[1] >= 10]
+print(filteredComprehension)
--- Section-05/9-map-function/app.py
+++ Section-05/9-map-function/app.py
@@ -4,8 +4,15 @@ items = [
     ("Product3", 12)]
 
 
-prices = []
-for item in items:
-    prices.append(item[1])
+# prices = []
+# for item in items:
+#     prices.append(item[1])
 
-print(prices)
+# print(prices)
+
+prices = map(lambda item: item[1], items)
+for item in prices:
+    print(item)
+# Alternatively
+pricesList = list(map(lambda item: item[1], items))
+print(pricesList)
